2c1b871414074ddd720c1ee3b9af446caf72fe1c
miludCifp <10395@cifpceuta.es>
2024-02-08 17:24:22 +0100

    Primer juego unity3d

---
+++ Assets/Script/Movimiento_EjeX.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento_EjeX : MonoBehaviour
+{
+    private Rigidbody2D Rigidbody2D;
+    private float horizontal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        // almacena -1 si pulsamos tecla a
+        // almacena 0 si no pulsamos nada
+        // almacena 1 si pulsamos tecla d
+    }
+    void FixedUpdate()
+    {
+        Rigidbody2D.velocity = new Vector2(horizontal, Rigidbody2D.velocity.y);
+    }
+}
