db06d996c01e55f55f10fd0c0c5603e495cdb54c
miludCifp <10395@cifpceuta.es>
2024-02-29 10:39:38 +0100

    permitir que el enemigo dispare - Terminado

--- Assets/Script/GruntMovement.cs
+++ Assets/Script/GruntMovement.cs
@@ -5,32 +5,48 @@ using UnityEngine;
 public class GruntMovement : MonoBehaviour
 {
     public GameObject John;
+    public GameObject BulletPrefab;
+    private float LastShoot;
     private Rigidbody2D Rigidbody2D;
-    private float horizontal;
 
     // Start is called before the first frame update
     void Start()
     {
-        Rigidbody2D = GetComponent<Rigidbody2D>();
+        //Rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        // almacena -1 si pulsamos tecla a
-        // almacena 0 si no pulsamos nada
-        // almacena 1 si pulsamos tecla d
-
 
         Vector3 direction = John.transform.position - transform.position;
         // Cambiamos la dirección del enemigo para que mire a donde esta John
         if( direction.x >= 0.0f) transform.localScale = new Vector3(1.0f,1.0f,1.0f);
         else transform.localScale = new Vector3(-1.0f,1.0f,1.0f);
         // Recordad hacer un Freeze Rotation del eje Z para que no rote
+
+
+        //Medimos la distancia entre los dos personajes
+        float distance = Mathf.Abs(John.transform.position.x - transform.position.x);
+
+        // si la distancia es menor que 1 metro entonces permitimos al enemigo disparar
+        // Le añadimos un delay tal como habíamos hecho con el disparo de John
+        if (distance < 1.0f && Time.time > LastShoot + 0.25f)
+        {
+            Shoot();
+            LastShoot = Time.time;
+        }
+
+
+    }
+    private void Shoot()
+    {
+        Vector3 direction = new Vector3(transform.localScale.x, 0.0f, 0.0f);
+        GameObject bullet = Instantiate(BulletPrefab, transform.position + direction * 0.1f, Quaternion.identity);
+        bullet.GetComponent<BulletScript>().SetDirection(direction);
     }
     void FixedUpdate()
     {
-        Rigidbody2D.velocity = new Vector2(horizontal, Rigidbody2D.velocity.y);
+        //Rigidbody2D.velocity = new Vector2(horizontal, Rigidbody2D.velocity.y);
     }
 }
