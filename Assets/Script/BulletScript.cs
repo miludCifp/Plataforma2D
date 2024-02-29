437ff2e5490109b943ed63c251ed86afc1a666fe
miludCifp <10395@cifpceuta.es>
2024-02-29 11:23:59 +0100

    Efecto de sonido en bala- Terminado

--- Assets/Script/BulletScript.cs
+++ Assets/Script/BulletScript.cs
@@ -7,11 +7,14 @@ public class BulletScript : MonoBehaviour
     public float Speed;
     private Rigidbody2D Rigidbody2D;
     private Vector2 Direction;
-    // Start is called before the first frame update
+    public AudioClip sonido;
 
+
+    // Start is called before the first frame update
     void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
+        Camera.main.GetComponent<AudioSource>().PlayOneShot(sonido);
     }
 
     // Update is called once per frame
