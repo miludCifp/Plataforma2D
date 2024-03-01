1b66dc51008475f3f06a03e13956400802309484
miludCifp <10395@cifpceuta.es>
2024-03-01 18:55:01 +0100

    Modificación Segunda Escena

--- Assets/Script/BarraVida.cs
+++ Assets/Script/BarraVida.cs
@@ -9,42 +9,24 @@ public class BarraVida : MonoBehaviour
     public float vidaActual;
     public float vidaMaxima;
 
-    public Text txtVidaActual;
-
     // Update is called once per frame
     void Update()
     {
         //barraVd.fillAmount = vidaActual / vidaMaxima;
     }
 
-/*
+
     public void restarVida(){
         vidaActual = vidaActual - 20;
 
         float vdActual = barraVd.fillAmount = vidaActual / vidaMaxima;
 
         // Cargar aqui la vida actual en el objeto Text
-
-        txtVidaActual = FindObjectOfType<TxtVidaActual>();
-
-        txtVidaActual.Text(vdActual.toString());
+        //txtVidaActual = FindObjectOfType<TxtVidaActual>();
+        //txtVidaActual.text = "Vida actual: " + vidaActual.ToString();
 
 
         Debug.Log("Vida actual :  " + vidaActual);
     }
-    */
-
-
-
-    public void restarVida()
-    {
-        vidaActual -= 20f;
-        float vdActual = vidaActual / vidaMaxima;
-        barraVd.fillAmount = vdActual;
-
-        // Cargar aquí la vida actual en el objeto Text
-        txtVidaActual.text = "Vida actual: " + vidaActual.ToString();
-
-        Debug.Log("Vida actual: " + vidaActual);
-    }
+    
 }
